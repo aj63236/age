c8716c190a6dd1390c9aeeec3fa5bcba74c873d8
aj63236 <59002970+aj63236@users.noreply.github.com>
2021-12-05 01:50:50 +0800

    Update age.py

--- age.py
+++ age.py
@@ -1,12 +1,17 @@
 driving = input("你有沒有開過車(1:有 , 2: 沒有):")
 age = input("請問你現在幾歲:")
-driving = int(driving)
+#driving = int(driving)
 age = int(age)
 
-if driving == 1:
+if driving == '有':
 	if age >= 18 :
 		print("恭喜妳通過測驗")
 	else:
 		print("你這個罪犯")
+elif driving == '沒有':
+	if age >= 18:
+		print("太可惜了")
+	else:
+		print("再過幾年就能開車了")
 else:
-	print("太可惜了")
+	print("請輸入有或沒有")
